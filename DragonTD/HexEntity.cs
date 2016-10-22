ba0ea14d3f45934c58ec624ac59fa39386508240
Aaron Ertner <alertner+code@gmail.com>
2016-10-21 21:47:57 -0700

    Added hex entity

---
+++ DragonTD/HexEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DragonTD
+{
+    abstract class HexEntity : DrawableGameComponent
+    {
+        ///2D position in array for pathfinding
+        public Point Position;
+        
+        ///2D position of entity on screen for drawing
+        public Vector2 ScreenPosition;
+
+        public HexEntity(Game game) : base(game)
+        {
+
+        }
+        
+    }
+}
