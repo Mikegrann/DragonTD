279e035af73970bd1b93b495d6d1f94bbf3ebc31
Aaron Ertner <alertner+code@gmail.com>
2016-10-22 00:32:13 -0700

    Initial Tower code. May god have mercy on those who gaze upon this code.

---
+++ DragonTD/Tower/BasicTower.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace DragonTD.Tower
+{
+    class BasicTower : ProjectileTower
+    {
+        TowerStats L1Stats = new TowerStats(128f, 5);
+        TowerStats L2Stats = new TowerStats(192f, 10);
+        TowerStats L3Stats = new TowerStats(256f, 20);
+        
+        Texture2D TowerTexture;
+
+        public BasicTower(Game game, Point position, Texture2D towerTexture, Texture2D projectileTexture) : base(game, position, towerTexture)
+        {
+            
+        }
+    }
+}
